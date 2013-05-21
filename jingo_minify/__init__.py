7ffde865019d7e15df6a9a3d06116281a6935516
Shuhao Wu <shuhao@shuhaowu.com>
2013-05-21 13:57:56 -0700

    Fixed PEP8

--- jingo_minify/__init__.py
+++ jingo_minify/__init__.py
@@ -22,21 +22,21 @@ def check_css():
                 elif item.endswith('.styl'):
                     stylus_found = True
 
-        valid_less = getattr(settings, 'LESS_BIN', False) and \
-                     getattr(settings, "LESS_PREPROCESS", False)
+        valid_less = (getattr(settings, 'LESS_BIN', False) and
+                      getattr(settings, "LESS_PREPROCESS", False))
         valid_sass = getattr(settings, 'SASS_BIN', False)
         valid_stylus = getattr(settings, 'STYLUS_BIN', False)
 
         if less_found and not valid_less:
-            print 'Warning: LESS_BIN not found or LESS_PREPROCESS is ' \
-                  'False/undefined but less files are being used!'
+            print ('Warning: LESS_BIN not found or LESS_PREPROCESS is '
+                   'False/undefined but less files are being used!')
 
         if sass_found and not valid_sass:
             print 'Warning: SASS_BIN not found but sass files are being used!'
 
         if stylus_found and not valid_stylus:
-            print 'Warning: STYLUS_BIN not found but stylus files are ' \
-                  'being used!'
+            print ('Warning: STYLUS_BIN not found but stylus files are '
+                   'being used!')
 
 
 check_css()
