aadbe464ed9b97bf673522e8b8e518393dec9c15
Shuhao Wu <shuhao@shuhaowu.com>
2013-05-21 14:21:45 -0700

    Reworked, test coming next

--- jingo_minify/__init__.py
+++ jingo_minify/__init__.py
@@ -1,13 +1,14 @@
 import os
-
-from django.conf import settings
-
-VERSION = (0, 6, 0)
-__version__ = '.'.join((str(x) for x in VERSION))
-
+import warnings
 
 def check_css():
-    if settings.DEBUG:
+    try:
+        from django.conf import settings
+    except ImportError:
+        return []
+    else:
+        w = []
+
         less_found = False
         sass_found = False
         stylus_found = False
@@ -28,16 +29,21 @@ def check_css():
         valid_stylus = getattr(settings, 'STYLUS_BIN', False)
 
         if less_found and not valid_less:
-            print ('Warning: LESS_BIN not found or LESS_PREPROCESS is '
-                   'False/undefined but less files are being used!')
+            w.append('LESS_BIN not found or LESS_PREPROCESS is '
+                     'False/undefined but less files are being used!')
 
         if sass_found and not valid_sass:
-            print 'Warning: SASS_BIN not found but sass files are being used!'
+            w.append('SASS_BIN not found but sass files are being used!')
 
         if stylus_found and not valid_stylus:
-            print ('Warning: STYLUS_BIN not found but stylus files are '
-                   'being used!')
+            w.append('STYLUS_BIN not found but stylus files are being used!')
+
+        return w
+
+for msg in check_css():
+    warnings.warn(msg)
+
+VERSION = (0, 6, 0)
+__version__ = '.'.join((str(x) for x in VERSION))
 
 
-check_css()
-del check_css
